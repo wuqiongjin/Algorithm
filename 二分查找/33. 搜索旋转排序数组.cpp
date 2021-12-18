330623681e97fbbf718797d478d165a449ccb0ed
wuqiongjin <1301492906@qq.com>
2021-12-18 08:45:20 +0800

    二分找2个端点N-2

--- 二分查找/33. 搜索旋转排序数组.cpp
+++ 二分查找/33. 搜索旋转排序数组.cpp
@@ -37,3 +37,47 @@ public:
         return -1;
     }
 };
+
+//利用旋转点去进行2次二分查找
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        //先通过一次二分查找找出旋转点，然后确定旋转点后，再在有序区间内去进行 普通的二分查找
+        int left = 0, right = nums.size() - 1;
+        while(left < right)
+        {
+            int mid = (right - left) / 2 + left + 1;
+            if(nums[mid] >= nums[0])
+                left = mid;
+            else
+                right = mid - 1;
+        }
+
+        //判断target在哪个区间
+        if(target >= nums[0])
+        {
+            left = 0;
+        }
+        else
+        {
+            left = left + 1;
+            right = nums.size() - 1;
+        }
+
+        while(left < right)
+        {
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] >= target)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        //其实这里推荐用right作为结果值的，因为left可能会越界，自己试试就知道了
+        //我这里就多加了个判断
+        if(left >= nums.size())
+            return - 1;
+        return (nums[left] == target ? left : -1);
+    }
+};
