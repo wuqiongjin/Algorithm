47e794d8cb9f644ab3ef5ab100f6823ea94cac1e
wuqiongjin <1301492906@qq.com>
2022-06-09 20:59:03 +0800

    N-2 电话号码的字母组合

--- 回溯算法/17. 电话号码的字母组合.cpp
+++ 回溯算法/17. 电话号码的字母组合.cpp
@@ -45,3 +45,48 @@ public:
     string path;        //单次遍历结果
     vector<string> ans; //结果集合
 };
+
+
+//N-2
+class Solution {
+public:
+    unordered_map<char,string> umcs = {
+        {'2', "abc"},
+        {'3', "def"},
+        {'4', "ghi"},
+        {'5', "jkl"},
+        {'6', "mno"},
+        {'7', "pqrs"},
+        {'8', "tuv"},
+        {'9', "wxyz"}
+    };
+
+    void backtracking(int digitsIndex, const string& digits){
+        if(digitsIndex == digits.size()){
+            res.push_back(path);
+            return;
+        }
+
+        string word = umcs[digits[digitsIndex]];//设每个数字字符对应一个word:"abc"、"def"、...
+        //这里的i是每个word的下标
+        for(int i = 0; i < word.size(); ++i)//我们需要横向遍历的是每个word, 纵向遍历的是digits
+        {                                   //横向遍历是for循环处理,纵向遍历是递归处理
+            path.push_back(word[i]);
+            backtracking(digitsIndex + 1, digits);
+            path.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits == ""){
+            return {};
+        }
+        res.clear();
+        path = "";
+        backtracking(0, digits);
+        return res;
+    }
+
+    vector<string> res;
+    string path;
+};
