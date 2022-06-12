b995164f6d4cfed00012c899a14e2c56eb74fe5c
wuqiongjin <1301492906@qq.com>
2022-06-12 16:49:09 +0800

    N-2

--- 回溯算法/131. 分割回文串.cpp
+++ 回溯算法/131. 分割回文串.cpp
@@ -44,3 +44,54 @@ public:
     vector<string> path;
     vector<vector<string>> ans;
 };
+
+
+//N-2
+class Solution {
+public:
+    //1. 切割问题是如何转化为组合问题的 (每次切割完一部分后, 下一次就是从剩余的部分进行再切割, 组合也是)
+    //2. 如何进行切割? (使用下标startIndex控制切割的起始位置, i控制控制位置)
+    //3. 如何判断切割完成了? (startIndex到达了字符串的结尾)
+    //4. 如何截取切割的字符串 (在进行递归的时候使用[startIndex, i]进行切割)
+    //5. 如何判断回文字符串
+    bool isPalindrome(const string& s, int start, int end){
+        while(start < end)
+        {
+            if(s[start] != s[end]){
+                return false;
+            }
+            ++start;
+            --end;
+        }
+        return true;
+    }
+    
+    void backtracking(int startIndex, const string& s){
+        if(startIndex >= s.size()){
+            res.push_back(path);
+            return;
+        }
+
+        for(int i = startIndex; i < s.size(); ++i)
+        {
+            if(isPalindrome(s, startIndex, i)){
+                path.push_back(s.substr(startIndex, i - startIndex + 1));
+            }
+            else{
+                continue;
+            }
+            backtracking(i + 1, s);
+            path.pop_back();	//回溯, 继续找下一个切割点
+        }
+    }
+    
+    vector<vector<string>> partition(string s) {
+        path.clear();
+        res.clear();
+        backtracking(0, s);
+        return res;
+    }
+    
+    vector<string> path;
+    vector<vector<string>> res;
+};
--- 回溯算法/93. 复原IP地址.cpp
+++ 回溯算法/93. 复原IP地址.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isValid(const string& s)
     {
-        if(s.size() < 1 || s.size() > 3)    //检查位数
+        if(s.size() < 1 || s.size() > 3)    //检查位数(不然有可能超出int的范围)
             return false;
         else
         {
@@ -50,4 +50,72 @@ public:
     }
 
     vector<string> ans;
+};
+
+
+//N-2
+class Solution {
+public:
+    bool isValid(const string& s)
+    {
+        //0 ~ 255
+        if(s.size() < 1 || s.size() > 3){   //这里必须要这样判断一下, 不然有可能超出int范围
+            return false;
+        }
+
+        if(stoi(s) < 0 || stoi(s) > 255){
+            return false;
+        }
+
+        //不能有前导0
+        if(s == "0"){
+            return true;
+        }
+
+        for(int i = 0; i < s.size(); ++i)
+        {
+            if(s[i] != '0'){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return false;
+    }
+
+    void backtracking(int startIndex, string& s, int pointNum){
+        if(pointNum == 3){
+            string tmp = s.substr(startIndex);	//单独处理一下最后第四部分IP段
+            if(isValid(tmp)){
+                res.push_back(s);
+            }
+            return;
+        }
+
+        for(int i = startIndex; i < s.size(); ++i)
+        {
+            string ret = s.substr(startIndex, i - startIndex + 1);
+            if(isValid(ret)){
+                s.insert(s.begin() + i + 1, '.');   //在i位置的后面添加".", 所以是i+1
+                ++pointNum;
+                backtracking(i + 2, s, pointNum);
+                s.erase(s.begin() + i + 1);
+                --pointNum;
+            }
+        }
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        if(s.size() < 4 || s.size() > 12){
+            return {};
+        }
+        // path = "";
+        res.clear();
+        backtracking(0, s, 0);
+        return res;
+    }
+
+    // string path; //还是直接在s上修改比较方便, 这样的做法麻烦, 在pop_back的时候还要把之前添加进去的字符串也pop, 也就是需要pop2次...
+    vector<string> res;
 };
