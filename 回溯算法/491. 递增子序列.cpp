d06faca71700c8d69089248c00dd8b6a5a5e21e7
wuqiongjin <1301492906@qq.com>
2022-06-15 22:49:42 +0800

    add unordered_set ver

--- 回溯算法/491. 递增子序列.cpp
+++ 回溯算法/491. 递增子序列.cpp
@@ -32,3 +32,50 @@ public:
     vector<int> path;
     vector<vector<int>> ans;
 };
+
+
+//用了set (空间复杂度 不如 使用数组)
+class Solution {
+public:
+    //不用再写个函数去判断了
+    // bool isIncrement(){
+    //     for(int i = 0; i < path.size() - 1; ++i)
+    //     {
+    //         if(path[i] > path[i + 1]){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    void backtracking(int startIndex, vector<int>& nums){
+        if(path.size() >= 2){
+            res.push_back(path);
+        }
+        if(startIndex >= nums.size()){
+            return;
+        }
+        unordered_set<int> us;  //每层(这个说法不完全准确,是对于一个父亲下面的同一层)独立创建一个set(树层去重)
+        for(int i = startIndex; i < nums.size(); ++i)
+        {
+            if((!path.empty() && nums[i] < path.back()) ||  //在这里直接用nums[i] < path.back()判断是否递增
+                us.find(nums[i]) != us.end()){
+                continue;        
+            }
+            path.push_back(nums[i]);
+            us.insert(nums[i]);
+            backtracking(i + 1, nums);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        path.clear();
+        res.clear();
+        backtracking(0, nums);
+        return res;
+    }
+
+    vector<int> path;
+    vector<vector<int>> res;
+};
