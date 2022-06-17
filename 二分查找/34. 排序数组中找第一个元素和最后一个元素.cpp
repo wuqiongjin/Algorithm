b273e816a2edbbf74691e05dbfe1ae2f6cec0674
wuqiongjin <1301492906@qq.com>
2022-06-17 16:27:37 +0800

    N-N 二分leftborder & rightborder

--- 二分查找/34. 排序数组中找第一个元素和最后一个元素.cpp
+++ 二分查找/34. 排序数组中找第一个元素和最后一个元素.cpp
@@ -1,5 +1,76 @@
-//这里找的区间是[left, right]
+//N - N刷了... (这个思路最清晰!)
+class Solution {
+public:
+    int findLeft(vector<int>& nums, int target){
+        int left = 0, right = nums.size() - 1;
+        while(left <= right)
+        {
+            int mid = left + ((right - left) >> 1);
+            if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{   //nums[mid] == target
+                if(mid > 0 && nums[mid - 1] == target){ //判断mid的前一个元素是否还是target
+                    right = mid - 1;
+                }
+                else{
+                    return mid;
+                }
+            }
+        }
+        return -1;  //走到这里说明没找到, 直接返回-1 (如果left返回-1, 那么right一定也会返回-1)
+    }
 
+    int findRight(vector<int>& nums, int target){
+        int left = 0, right = nums.size() - 1;
+        while(left <= right)
+        {
+            int mid = left + ((right - left) >> 1);
+            if(nums[mid] < target){
+                left = mid + 1;
+            }
+            else if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{	//nums[mid] == target
+                if(mid + 1 < nums.size() && nums[mid + 1] == target){   //判断mid的后一个元素是否还是target
+                    left = mid + 1;
+                }
+                else{
+                    return mid;
+                }
+            }
+        }
+        return -1;  //走到这里说明没找到, 直接返回-1
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.empty()){
+            return {-1, -1};
+        }
+        int left = findLeft(nums, target);
+        int right = findRight(nums, target);
+
+        return {left, right};   //直接返回就行了, 这里的left和right包含了找不到的情况
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//N-1
+//这里找的区间是[left, right]
 class Solution {
 public:
     //思路：分开寻找Left左边界和Right右边界
