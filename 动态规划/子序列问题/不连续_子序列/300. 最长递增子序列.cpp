a4c21ea14453f18838fb1b5658de26382321e5e2
wuqiongjin <1301492906@qq.com>
2022-07-09 23:53:02 +0800

    N-2 最长递增子序列

--- 动态规划/子序列问题/不连续_子序列/300. 最长递增子序列.cpp
+++ 动态规划/子序列问题/不连续_子序列/300. 最长递增子序列.cpp
@@ -21,3 +21,27 @@ public:
         return ans;
     }
 };
+
+
+//N-2	注释很有参考意义哦!
+class Solution {
+public:
+    //dp[i]: 从0~i之间截取序列, 并以i为递增子序列的结尾元素的最长子序列的长度为dp[i]
+    //"位置i的最长升序子序列等于 j从 0 到 i-1 各个位置的最长升序子序列 + 1 的最大值。"
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        int res = 1;
+
+        for(int i = 1; i < nums.size(); ++i)
+        {
+            for(int j = 0; j <= i - 1; ++j)
+            {
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j] + 1);  //这里取max并不是比较的含义, 而是让dp[i]保存 j从0到i-1遍历过程中的最大值!
+                }
+            }
+            res = max(res, dp[i]);  //dp[nums.size() - 1]的位置并不一定是最长的子序列(注意下dp数组的含义)
+        }
+        return res;
+    }
+};
