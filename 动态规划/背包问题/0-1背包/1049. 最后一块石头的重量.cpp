1283190ba39710015d133fe87b24066e7051ed18
wuqiongjin <1301492906@qq.com>
2022-07-16 00:14:31 +0800

    转化题目->01背包

--- 动态规划/背包问题/0-1背包/1049. 最后一块石头的重量.cpp
+++ 动态规划/背包问题/0-1背包/1049. 最后一块石头的重量.cpp
@@ -24,3 +24,31 @@ public:
         //return sum - 2 * dp[target];  //向下取整
     }
 };
+
+
+//N-2
+class Solution {
+public:
+    //思路:
+    //求最小可能的重量, 我们可以设想这个数组排过序后, 什么情况下的组合能让重量最小?
+    //我们可以把这组数只看做2个数, 即, 把他们分成两组(每组就一个石头[求和])
+    //设这组数的和为sum, 那么如果一部分的和最接近sum/2, 那么另一部分的和也是最接近sum/2的, 这样的话它们互相抵消后,
+    //就保证了重量最小
+    //因此, 问题转化为了, 如何求最接近sum/2的和 (即如何让背包容量为sum/2的背包最满)
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for(int e : stones)
+            sum += e;
+        int target = sum / 2; //向下取整 (保证 target <= sum / 2 即, sum - dp[target] - dp[target] >= 0)
+        vector<int> dp(target + 1, 0);
+            
+        for(int i = 0; i < stones.size(); ++i)
+        {
+            for(int j = target; j >= stones[i]; --j)
+            {
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+        return sum - 2 * dp[target];   //求大于sum/2的那个和 - 小于sum/2的那个和
+    }
+};
